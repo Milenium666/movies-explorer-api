395ae35c43ea6f8ff22004ad49a13777dbf47c72
Milenium666 <whiteangel64rus@gmail.com>
2021-11-29 14:58:56 +0300

    реализовать авторизацию и аутентификацию

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,28 @@
-const bcrypt = require('bcryptjs')
+//!!! найти информацию EN
+
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
 const {
   OK,
   salt,
   RE_REGISTRATION,
   INCORECT_DATA,
   SERVER_ERROR,
+  THE_FIELD_IS_NOT_FILLED,
+  INCORRECT_EMAIL_AND_PASSWORD,
+  JWT_KEY_SEKRET,
 } = require('../utils/constans');
-const RepeatRegistEmail = require('../error/RepeatRegistEmail');
 
+const RepeatRegistEmail = require('../error/RepeatRegistEmail');
+const IncorectData = require('../error/IncorectData');
+const ServerError = require('../error/ServerError');
+const IncorrectEmailAndPass = require('../error/IncorrectEmailAndPass');
+
+//!!решить баг если поле не заполнено(после установки joi)
 const createUser = (req, res, next) => {
   const { email, password, name } = req.body
   User.findOne({ email })
@@ -33,9 +46,34 @@ const createUser = (req, res, next) => {
     })
     .catch(next);
 };
-const login = (req, res, next) => {
 
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(new IncorectData(THE_FIELD_IS_NOT_FILLED));
+  } User.findOne({email}).select('+password')
+      .then((user) => {
+        if(!user) {
+          next(new IncorrectEmailAndPass(INCORRECT_EMAIL_AND_PASSWORD));
+        } else {
+          return bcrypt.compare(password, salt, (isValid) => {
+            if(!isValid) {
+              next(new IncorrectEmailAndPass(INCORRECT_EMAIL_AND_PASSWORD))
+            } else {
+              const token = jwt.sign({ id: user._id },
+              JWT_KEY_SEKRET,
+              { expiresIn: '7d' });
+              return res.status(OK).send({ token });
+              }
+          })
+        }
+      })
+      .catch(() => {
+        next(new ServerError(SERVER_ERROR));
+      });
 };
+
 const getInfoUser = (req, res, next) => {
 
 };
--- utils/constans.js
+++ utils/constans.js
@@ -1,9 +1,13 @@
 const OK = 200;
 const salt = 10;
 
+const JWT_KEY_SEKRET = 'super-strong-secret';
+
 const RE_REGISTRATION = 'Пользователь с таким Email уже есть в системе';
 const INCORECT_DATA = 'Введены некрректные данные при создании пользователя';
 const SERVER_ERROR = 'Ошибка на стороне сервера';
+const THE_FIELD_IS_NOT_FILLED = 'Поле не заполнено';
+const INCORRECT_EMAIL_AND_PASSWORD = 'Передан неверный Email или Пароль';
 
 module.exports = {
   OK,
@@ -11,4 +15,7 @@ module.exports = {
   RE_REGISTRATION,
   INCORECT_DATA,
   SERVER_ERROR,
+  THE_FIELD_IS_NOT_FILLED,
+  INCORRECT_EMAIL_AND_PASSWORD,
+  JWT_KEY_SEKRET,
 }
